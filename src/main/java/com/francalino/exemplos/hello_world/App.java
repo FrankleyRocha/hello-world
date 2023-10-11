a128d45fa2340f68c2bbec365530ba247f394f74
Frankley Francalino da Rocha <rocha112358@gmail.com>
2023-10-10 21:03:55 -0300

    primeiro commit

---
+++ src/main/java/com/francalino/exemplos/hello_world/App.java
@@ -0,0 +1,13 @@
+package com.francalino.exemplos.hello_world;
+
+/**
+ * Hello world!
+ * @author Frankley Francalino da Rocha
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Olá Mundo!" );
+    }
+}
